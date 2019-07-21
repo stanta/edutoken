9c220462b46f22f074ed11a25bf5e0bf0660e5ee
stanta <st.takt@gmail.com>
2019-07-21 22:42:56 +0300

    second commit plan of contract

---
+++ ERC223/edutoken.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.0;
+import 'ERC223/token/ERC223/ERC223_token.sol';
+
+contract edutoken is ERC223 {
+
+    mapping (address => uint) Tokenholders;
+
+    struct FeedBack {
+
+        uint: time;
+        uint8: balls:
+        bytes[256]: text;
+
+    }
+
+    FeedBack[] feedbacks;
+
+
+    function addFeedback() public onlyTokenholder {
+
+
+    }
+
+    function profitShare ()  public onlyTokenholder // shares income
+    {
+
+    }
+
+
+    function edutoken(){
+
+        }
+}
