faf7461fcde3aba71ce55564adcdd1c2fa20258b
stanta <st.takt@gmail.com>
2019-07-24 08:31:05 +0300

    added fields of extended feedback

--- ERC223/edutoken.sol
+++ ERC223/edutoken.sol
@@ -7,17 +7,28 @@ contract edutoken is ERC223 {
 
     struct FeedBack {
 
-        uint: time;
-        uint8: balls:
-        bytes[256]: text;
+        address sender;
+        uint time;
+        uint8 balls; // estimation of skills in 255 grades
+        bytes[256] org; //organisation where study, work or service
+        bytes32 code; //code of course study or work for codified  areas
+        bytes[256] descr ; //description of study oe work
+        bytes[256] text; //comment
 
     }
 
-    FeedBack[] feedbacks;
+    FeedBack[] FeedBacksJournal;
 
 
-    function addFeedback() public onlyTokenholder {
+    function addSimpleFeedback(uint8 _balls, bytes[256] _text) public onlyTokenholder {
+        feedback = FeedBack(msk.sender,now, _balls, "","", _text);
+        FeedBacksJournal.push(feedback);
 
+    }
+
+    function addExtendedFeedback(uint8 _balls,   bytes[256] _org, bytes32 _code,  bytes[256] _descr, bytes[256] _text) public onlyTokenholder {
+        feedback = FeedBack(msk.sender,now, _balls, _org, _code, _descr, _text);
+        FeedBacksJournal.push(feedback);
 
     }
 
